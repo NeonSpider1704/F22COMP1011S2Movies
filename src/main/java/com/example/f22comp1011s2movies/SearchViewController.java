6ca99616b4a687faeb05b4c3fb6813a8d04769fb
JaretWright <jaret.wright@georgiancollege.ca>
2022-11-23 11:33:17 -0500

    Week 11 - made the results box and msg label invisible on initial launch

--- src/main/java/com/example/f22comp1011s2movies/SearchViewController.java
+++ src/main/java/com/example/f22comp1011s2movies/SearchViewController.java
@@ -2,6 +2,7 @@ package com.example.f22comp1011s2movies;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -9,7 +10,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class SearchViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchViewController implements Initializable {
 
     @FXML
     private ListView<Movie> listView;
@@ -39,4 +43,9 @@ public class SearchViewController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        resultsBox.setVisible(false);
+        msgLabel.setVisible(false);
+    }
 }
