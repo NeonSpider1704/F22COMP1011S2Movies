fd1cf0c873c3d174995b3ade4d9d4c1ebc31f9f5
JaretWright <jaret.wright@georgiancollege.ca>
2022-11-23 06:36:29 -0500

    Week 11 - updated the view and controller to have a better look and feel

--- src/main/java/com/example/f22comp1011s2movies/SearchViewController.java
+++ src/main/java/com/example/f22comp1011s2movies/SearchViewController.java
@@ -1,26 +1,42 @@
 package com.example.f22comp1011s2movies;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
-import java.io.IOException;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class SearchViewController {
 
     @FXML
-    private ListView<Movie> movieListView;
+    private ListView<Movie> listView;
+
+    @FXML
+    private Label msgLabel;
+
+    @FXML
+    private ImageView posterImageView;
+
+    @FXML
+    private HBox resultsBox;
 
     @FXML
     private TextField searchTextField;
 
     @FXML
-    private void search() throws IOException, InterruptedException {
-        APIUtility.getMoviesFromOMDB(searchTextField.getText());
-        APIResponse apiResponse = APIUtility.getMoviesFromFile();
-        movieListView.getItems().addAll(apiResponse.getSearch());
+    private VBox selectedVBox;
+
+    @FXML
+    void getDetails(ActionEvent event) {
+
     }
 
+    @FXML
+    void search(ActionEvent event) {
+
+    }
 
 }
-
